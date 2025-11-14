77ffb0a4ec134b3aa1c4b14018f7ea802d1e6cd3
jwever <fatguy140@gmail.com>
2025-11-13 16:58:14 -0800

    First Commit

--- src/main/kotlin/io/github/joshlha/partnershipproject/Main.kt
+++ src/main/kotlin/io/github/joshlha/partnershipproject/Main.kt
@@ -1,6 +1,7 @@
 package io.github.joshlha.partnershipproject
 
 import com.formdev.flatlaf.FlatDarkLaf
+import io.github.joshlha.partnershipproject.Main.Companion.logger
 import net.miginfocom.swing.MigLayout
 import org.slf4j.LoggerFactory
 import java.awt.Color
@@ -10,77 +11,21 @@ import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.JPanel
 
-/*
-    "Main" is a subclass of JPanel. It gives JPanel's constructor a new MigLayout instance.
-    This tells the JPanel to use MigLayout for laying out the components.
-    There are many other layouts available in swing, but Mig is most used.
- */
-class Main : JPanel(MigLayout("ins 4, fill")) {
-    // Initialize a bunch of components
-    private val northButton = JButton("North!")
-    private val southButton = JButton("South!")
-    private val eastButton = JButton("East!")
-    private val westButton = JButton("West!")
 
-    private val topLeft = JLabel("Top Left").apply {
-        border = BorderFactory.createLineBorder(Color.GREEN)
-    }
-    private val topMiddle = JLabel("Top Middle").apply {
-        border = BorderFactory.createLineBorder(Color.BLUE)
-    }
-    private val topRight = JLabel("Top Right").apply {
-        border = BorderFactory.createLineBorder(Color.RED)
-    }
+fun main(args: Array<String>) {
+    FlatDarkLaf.setup() // Setup the Look-and-feel. This gives us a dark mode, modern UI
+    logger.info("Showing JFrame!")
 
-    private var number = 0
-
-    private val labelText: String
-        get() = "Click to increment number: $number"
-
-    private val bottom = JButton(labelText).apply {
-        border = BorderFactory.createLineBorder(Color.WHITE)
-        horizontalAlignment = JButton.CENTER
-
-        addActionListener {
-            number += 1
-            text = labelText
-        }
-    }
-
-    init {
-        // Add the components to the layout, providing MigLayout constraints for their position
-        add(northButton, "north")
-        add(southButton, "south")
-        add(eastButton, "east")
-        add(westButton, "west")
-        add(topLeft, "grow")
-        add(topMiddle, "grow")
-        add(topRight, "grow, wrap")
-        add(bottom, "grow, span")
-    }
-
-    // A singleton instance object called "Main" because it is the companion to the Main class.
-    companion object {
-        private val logger = LoggerFactory.getLogger(Main::class.java)
-
-        // This annotation is needed to tell the compiler that this is our java main function.
-        @JvmStatic
-        fun main(args: Array<String>) {
-            FlatDarkLaf.setup() // Setup the Look-and-feel. This gives us a dark mode, modern UI
-            logger.info("Showing JFrame!")
-
-            // Create an instance of JFrame and set it visible to show it.
-            // A JFrame is effectively just a window.
-            // Set the "contentPane" to an instance of the "Main" class, which is a customized JPanel.
-            JFrame("Partnership Program").apply {
-                contentPane = Main()
-                setSize(800, 600)
-                setLocationRelativeTo(null) // Centers the JFrame on the screen.
-                isVisible = true // false by default.
-                defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
-                // Disposing one frame removes it and its children from memory.
-                // Disposing all frames ends the program.
-            }
-        }
+    // Create an instance of JFrame and set it visible to show it.
+    // A JFrame is effectively just a window.
+    // Set the "contentPane" to an instance of the "Main" class, which is a customized JPanel.
+    JFrame("Partnership Program").apply {
+        contentPane = Main()
+        setSize(800, 600)
+        setLocationRelativeTo(null) // Centers the JFrame on the screen.
+        isVisible = true // false by default.
+        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+        // Disposing one frame removes it and its children from memory.
+        // Disposing all frames ends the program.
     }
 }
---
+++ src/main/kotlin/io/github/joshlha/partnershipproject/PanelView.kt
@@ -0,0 +1,174 @@
+package io.github.joshlha.partnershipproject
+
+import net.miginfocom.swing.MigLayout
+import org.slf4j.LoggerFactory
+import java.awt.Color
+import javax.swing.BorderFactory
+import javax.swing.JButton
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+/*
+    "Main" is a subclass of JPanel. It gives JPanel's constructor a new MigLayout instance.
+    This tells the JPanel to use MigLayout for laying out the components.
+    There are many other layouts available in swing, but Mig is most used.
+ */
+
+
+// TODO only allow single decimal at a time --- DONE
+// TODO create a del and a reset button --- DONE
+// TODO make a variable for the label value --- DONE
+class Main : JPanel(MigLayout("debug, ins 4, fill", "[fill, sg g]5", "")) {
+
+    private var nextOperator: String? = null
+    private var operatorFlag: Boolean = false
+
+    private var firstOperand = "0.0"
+    private var secondOperand = "0.0"   // 1 + 2
+        set(value) {
+            field = value
+            valueLabel.text = value
+        }
+
+    private fun digitButtonStyle(text: String) = JButton(text).apply {
+            horizontalAlignment = JButton.CENTER
+            border = BorderFactory.createLineBorder(Color.GRAY)
+            font = font.deriveFont(24f)
+
+        addActionListener {
+
+            if (operatorFlag) {
+                // TODO set current value to previous value, and clear current value
+                firstOperand = secondOperand
+                operatorFlag = false
+                secondOperand = text
+            } else {
+                if (secondOperand == "0.0") {
+                    secondOperand = text
+                }
+                else if(!secondOperand.contains(".") || text != ".") {
+                    secondOperand = secondOperand + text
+                }
+            }
+
+
+
+        }
+    }
+
+    private fun operatorButtonStyle(text: String) = JButton(text).apply {
+        background = Color.ORANGE
+        foreground = Color.BLACK
+        font = font.deriveFont(24f)
+
+        addActionListener {
+            // run calculation
+            if (nextOperator == null) {
+                nextOperator = text
+                operatorFlag = true
+            //
+            } else if(!operatorFlag) {
+                if (text != "=") {
+                    nextOperator = text
+                }
+                runCalculation()
+                operatorFlag = true
+            } else {
+                nextOperator = text
+            }
+
+//            when (text) {
+//                "C" -> nextOperator = null
+//                "del" -> nextOperator = null
+//                "=" -> {
+//
+//                    // TODO calculate new value
+//
+//                    nextOperator = null
+//                }
+//                else -> nextOperator = text
+//
+//            }
+            // if operator is null then do nothing if pressing =
+            // otherwise if there is an operator queued
+        }
+
+    }
+
+    fun runCalculation() {
+        var calSum: Double = 0.0
+
+        when (nextOperator) {
+            "+" -> calSum = firstOperand.toDouble() + secondOperand.toDouble()
+            "-" -> calSum = firstOperand.toDouble() - secondOperand.toDouble()
+            "*" -> calSum = firstOperand.toDouble() * secondOperand.toDouble()
+            "/" -> calSum = firstOperand.toDouble() / secondOperand.toDouble()  //TODO check for / by zero
+
+        }
+        secondOperand = calSum.toString()
+        valueLabel.text = calSum.toString()
+        firstOperand = "0.0"
+    }
+
+
+    private val valueLabel = JLabel("0.0").apply {
+        border = BorderFactory.createLineBorder(Color.WHITE)
+        horizontalAlignment = JButton.LEFT
+        background = Color.WHITE
+        font = font.deriveFont(24f)
+        horizontalAlignment = JLabel.RIGHT
+
+    }
+
+    init {
+        // Add the components to the layout, providing MigLayout constraints for their position
+        // There is and implied "this." added before the "add" function call
+        add(valueLabel, "span 5, grow, wrap")
+
+
+        add(digitButtonStyle("7"), "grow")
+        add(digitButtonStyle("8"), "grow")
+        add(digitButtonStyle("9"), "grow")
+
+        add(operatorButtonStyle("del").apply {
+            addActionListener {
+                secondOperand = secondOperand.dropLast(1)
+                if (secondOperand == "") {
+                    secondOperand = "0.0"
+                }
+            }
+        }, "span 2, grow, wrap")
+
+        add(digitButtonStyle("4"), "grow")
+        add(digitButtonStyle("5"), "grow")
+        add(digitButtonStyle("6"), "grow")
+        add(operatorButtonStyle("*"), "grow")
+        add(operatorButtonStyle("/"), "grow, wrap")
+
+        add(digitButtonStyle("1"), "grow") //w 10:200:300
+        add(digitButtonStyle("2"), "grow")
+        add(digitButtonStyle("3"), "grow")
+        add(operatorButtonStyle("-"), "grow")
+        add(operatorButtonStyle("="), "span 1 2, grow, wrap")
+
+        add(operatorButtonStyle("C").apply {
+            addActionListener {
+                secondOperand = "0.0"
+            }
+        }, "grow")
+
+        add(digitButtonStyle("0"), "grow")
+        add(digitButtonStyle("."), "grow")
+        add(operatorButtonStyle("+"), "grow")
+//        add(operatorButtonStyle("del"), "grow, wrap")
+    }
+
+    // A singleton instance object called "Main" because it is the companion to the Main class.
+    companion object {
+        public val logger = LoggerFactory.getLogger(Main::class.java)
+
+
+    }
+}
+
+
